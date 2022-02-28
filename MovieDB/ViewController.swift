d11d3cf1aab50f51c93e7020755219c3d463c4dc
Damir Yackupov <yackupov.damir@gmail.com>
2022-03-01 01:34:56 +0300

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Damir Yackupov on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
